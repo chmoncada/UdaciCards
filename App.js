1196efa4932dec6932b5d4b965c3ef6646a385b8
Charles Moncada <chmoncada@me.com>
2018-02-19 11:05:31 -0300

    feat: Add Tabbar, Icons and mockViews

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, Platform } from 'react-native'
+import { TabNavigator } from 'react-navigation'
+import { MaterialCommunityIcons, Entypo } from '@expo/vector-icons'
+import { green, white } from './utils/colors'
+import { Constants } from 'expo'
+import Decks from './components/Decks'
+import NewDeck from './components/NewDeck'
+
+const Tabs = TabNavigator({
+    Decks: {
+        screen: Decks,
+        navigationOptions: {
+            tabBarLabel: 'Decks',
+            tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='cards-outline' size={30} color={tintColor} />
+        }
+    },
+    NewDeck: {
+        screen: NewDeck,
+        navigationOptions: {
+            tabBarLabel: 'New Deck',
+            tabBarIcon: ({ tintColor }) => <Entypo name='add-to-list' size={30} color={tintColor} />
+        }
+    },
+}, {
+    navigationOptions: {
+        header: null
+    },
+    tabBarOptions: {
+        activeTintColor: Platform.OS === 'ios' ? green : white,
+        style: {
+            height: 56,
+            backgroundColor: Platform.OS === 'ios' ? white : green,
+            shadowColor: 'rgba(0, 0, 0, 0.24)',
+            shadowOffset: {
+                width: 8,
+                height: 3
+            },
+            shadowRadius: 6,
+            shadowOpacity: 1
+        }
+    }
+})
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{ flex: 1 }}>
+          <View style={{ height: 20 }} />
+          <Tabs/>
       </View>
     );
   }
---
+++ components/NewDeck.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class NewDeck extends Component {
+    render() {
+        return (
+            <Text>NEW DECK</Text>
+        )
+    }
+}
+
+export default NewDeck
